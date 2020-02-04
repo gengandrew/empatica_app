5b0dc7bba3d1f7bd7aaad265d680b72470875c3f
Andrew Geng <gengandrew88@gmail.com>
2020-02-03 21:26:55 -0600

    update

--- app/src/main/java/com/empatica/application/retrofit/RetrofitClient.java
+++ app/src/main/java/com/empatica/application/retrofit/RetrofitClient.java
@@ -5,8 +5,8 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    // Live Machine [http://192.168.8.105:5000/api/] || Virtual Machine [http://10.0.2.2:5000/api/]
-    private static String baseUrl = "http://192.168.43.49:5000/api/";
+    // Live Machine [http://192.168.8.105:8006/api/] || Virtual Machine [http://10.0.2.2:8006/api/]
+    private static String baseUrl = "http://192.168.43.49:8006/api/";
 
     private static Retrofit instance = new Retrofit.Builder()
             .baseUrl(baseUrl)
